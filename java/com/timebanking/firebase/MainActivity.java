d351a2e1eca045ad468353991d9fd9e0dcbad67d
CletusAndoh <candoh97@gmail.com>
2018-04-15 15:44:38 -0400

    Updating Main Activity

--- java/com/timebanking/firebase/MainActivity.java
+++ java/com/timebanking/firebase/MainActivity.java
@@ -1,13 +1,28 @@
+
 package com.timebanking.firebase;
 
-import android.os.Bundle;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.timebanking.firebase.R;
 
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void gotoDesc(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DescriptionActivity.class);
+        startActivity(intent);
+    }
+
+
 }

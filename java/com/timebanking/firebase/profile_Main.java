4aee7964eea627a91029dd4df7dc463752acd217
xin tang <xtang100@syr.edu>
2018-05-01 21:31:47 -0400

    Update profile_Main.java

--- java/com/timebanking/firebase/profile_Main.java
+++ java/com/timebanking/firebase/profile_Main.java
@@ -3,8 +3,10 @@ package com.timebanking.firebase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,9 +15,16 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-
-
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 
 public class profile_Main extends AppCompatActivity {
@@ -23,73 +32,42 @@ public class profile_Main extends AppCompatActivity {
     TextView value;
     RatingBar rb;
     Button submitbtn;
-    EditText tv_name;
+    ListView tv_name;
     EditText tvaddress;
+    private DatabaseReference databaseReference;
+    private FirebaseDatabase mFirebaseInstance;
+    private FirebaseAuth mAuth;
+    long minutes, aminutes;
+    String userEmail, userId, userName;
+    ArrayList<String> minutesList  = new ArrayList<>();
+    ArrayList<String> nameList  = new ArrayList<>();
+    ArrayList<String> aminutesList  = new ArrayList<>();
+
+    // Editing values into database
 
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_profile);
-//
-//        profilePic = findViewById(R.id.ivProfilePic);
-//        profileName = findViewById(R.id.tvProfileName);
-//        profileAge = findViewById(R.id.tvProfileAge);
-//        profileEmail = findViewById(R.id.tvProfileEmail);
-//        profileUpdate = findViewById(R.id.btnProfileUpdate);
-//        changePassword = findViewById(R.id.btnChangePassword);
-//
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//
-//        firebaseAuth = FirebaseAuth.getInstance();
-//        firebaseDatabase = FirebaseDatabase.getInstance();
-//
-//        DatabaseReference databaseReference = firebaseDatabase.getReference(firebaseAuth.getUid());
-//
-//
-//        @Override
-//        public void onDataChange(DataSnapshot dataSnapshot) {
-//            UserProfile userProfile = dataSnapshot.getValue(UserProfile.class);
-//            profileName.setText("Name: " + userProfile.getUserName());
-//            profileAge.setText("Age: " + userProfile.getUserAge());
-//            profileEmail.setText("Email: " + userProfile.getUserEmail());
-//        }
-//
-//        @Override
-//        public void onCancelled(DatabaseError databaseError) {
-//            Toast.makeText(ProfileActivity.this, databaseError.getCode(), Toast.LENGTH_SHORT).show();
-//        }
-//    });
-//
-//        profileUpdate.setOnClickListener(new View.OnClickListener() {
-//        @Override
-//        public void onClick(View view) {
-//            startActivity(new Intent(ProfileActivity.this, UpdateProfile.class));
-//        }
-//    });
-//
-//        changePassword.setOnClickListener(new View.OnClickListener() {
-//        @Override
-//        public void onClick(View view) {
-//            startActivity(new Intent(ProfileActivity.this, UpdatePassword.class));
-//        }
-//    });
-//}
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//
-//        switch (item.getItemId()){
-//            case android.R.id.home:
-//                onBackPressed();
-//        }
-//        return super.onOptionsItemSelected(iteem);
-
-        // Rating ssytem
+    // Rating system
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
+//getting views
+        tv_name = (ListView) findViewById(R.id.testlistview);
+
+
+
+        mAuth = FirebaseAuth.getInstance();
+        final FirebaseUser user = mAuth.getCurrentUser();
+        userId = user.getUid();
+        userEmail = user.getEmail();
+        //getting the reference of artists node
+        databaseReference = FirebaseDatabase.getInstance().getReference("users");
+        addValueEventListener(databaseReference);
+
+
+
+
+        //list to store artists
 
         rb = (RatingBar) findViewById(R.id.ratingBar);
         value = (TextView) findViewById(R.id.value);
@@ -99,31 +77,76 @@ public class profile_Main extends AppCompatActivity {
             @Override
             public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
                 {
-                    value.setText("Value is " + rating );
+                    value.setText("Value is " + rating);
                 }
             }
         });
     }
+    private void addValueEventListener(final DatabaseReference userReference) {
+        /*add ValueEventListener to update data in realtime*/
+        userReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                /*this is called when first passing the data and
+                 * then whenever the data is updated*/
+                /*get the data children*/
+                Iterable<DataSnapshot> snapshotIterator = dataSnapshot.getChildren();
+                Iterator<DataSnapshot> iterator = snapshotIterator.iterator();
+                while (iterator.hasNext()) {
+                    String email;
+                    /*get the values as a user object*/
+                    User user = iterator.next().getValue(User.class);
 
-//    public void onButtonClickListener(){
-//
-//        rb = (RatingBar) findViewById(R.id.ratingBar);
-//        submitbtn = (Button) findViewById(R.id.button);
-//
-//        submitbtn.setOnClickListener(
-//           new View.OnClickListener(){
-//               @Override
-//               public void onClick(View v){
-//                   Toast.makeText(profile_Main.this,
-//                           String.valueOf(rb.getRating()),
-//                           Toast.LENGTH_SHORT).show();
-//               }
-//           }
-//        );
-//
-//
-//    }
+                    //One of the values we can get from authentication without accessing the database
+                    //is the current user email so I'm using it to find and access the current user's minutes
+                    //Once I get the minutes the uMinutes variable will be used to check that the user
+                    //has enough available minutes on their account before requesting a job.
+                    email = user.getEmail();
+                    if (email != null) {
+                        if (email.equals(userEmail)) {
+                            minutes = user.getMinutes();
+                            aminutes = user.getaMinutes();
+                            userName = user.getName();
+                        }
+                    }
+                    //These print statements are for testing and debugging purposes
+                }
+                nameList.add(userName);
+                ArrayAdapter nameAdapter = new ArrayAdapter<String>(profile_Main.this, android.R.layout.simple_list_item_1, nameList);
+                tv_name.setAdapter(nameAdapter);
+                System.out.println("Minutes :" + minutes);
+                System.out.println("Available Minutes :" + aminutes);
+                System.out.println("Name: " + userName);
 
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                /*listener failed or was removed for security reasons*/
+            }
+        });
+    }
+
+
+    public void onButtonClickListener() {
+
+        rb = (RatingBar) findViewById(R.id.ratingBar);
+        submitbtn = (Button) findViewById(R.id.button2);
+
+        submitbtn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(profile_Main.this,
+                                String.valueOf(rb.getRating()),
+                                Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+        );
+    }
 
 
 }
